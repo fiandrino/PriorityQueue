84b87ddd72b18dea170e4200409dd40345a6c9d1
Giorgio Fiandrino <giorgio.fiandrino@gmail.com>
2024-03-03 17:50:46 +0100

    Aggiungere i file di progetto.

---
+++ DRIVER/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PriorityQueue;
+
+namespace DRIVER
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PriorityQueue<string> p = new PriorityQueue<string>();
+        }
+    }
+}
---
+++ PriorityQueue/PriorityQueue.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PriorityQueue
+{
+    public class PriorityQueue<T> where T : class, IComparable<T>
+    {
+        private List<T> _heapTree;
+
+        public PriorityQueue()
+        {
+            _heapTree = new List<T>();
+        }
+
+        public void Enqueue(T item)
+        {
+            _heapTree.Add(item);
+            _heapTree.Sort();
+        }
+
+        public T Dequeue()
+        {
+            if (IsEmpty())
+            {
+                throw new InvalidOperationException("Queue is empty");
+            }
+
+            T item = _heapTree[0];
+            _heapTree.RemoveAt(0);
+            return item;
+        }
+
+        public bool IsEmpty()
+        {
+            return _heapTree.Count == 0;
+        }
+
+        public T Min()
+        {
+            if (IsEmpty())
+            {
+                throw new InvalidOperationException("Queue is empty");
+            }
+
+            return _heapTree[0];
+        }
+
+        public T Max()
+        {
+            if (IsEmpty())
+            {
+                throw new InvalidOperationException("Queue is empty");
+            }
+
+            return _heapTree[_heapTree.Count - 1];
+        }
+        public T Peek()
+        {
+            if (IsEmpty())
+            {
+                throw new InvalidOperationException("Queue is empty");
+            }
+
+            return _heapTree[0];
+        }
+    }
+}
